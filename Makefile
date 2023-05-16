f269568b34baf439ee0a2b9ce906b3067fbecb7a
Ruslan Shamsutdinov <nexus191@yandex.ru>
2023-05-17 02:11:42 +0500

    New project4

--- Makefile
+++ Makefile
@@ -15,6 +15,6 @@ test-coverage:
 dev:
 	poetry run flask --app page_analyzer:app run
 	
-PORT ?= 8000
+PORT ?= 5000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Это callable WSGI-приложение
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Welcome to Flask! Checking'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
